a2081ff0cdd2d175935fb73ea3bf21a975fa14ab
abiliojunior <abiliojunior.mat@gmail.com>
2017-12-24 21:42:00 -0300

    aula 5.29

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp  = require('gulp')
+    ,sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+    gulp.src('./src/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./src/css/'));
+})
+
