ecb3b50d9e1ea3a2d3ebd446f957d8fcd4897429
abiliojunior <abiliojunior.mat@gmail.com>
2018-01-06 14:38:13 -0300

    finalizado tarefas include do gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,48 @@
 var gulp  = require('gulp')
     ,sass = require('gulp-sass')
+    ,include = require ('gulp-file-include')
+    ,clean = require ('gulp-clean')
     ,browserSync = require('browser-sync');
+
+gulp.task('clean',function(){
+    return gulp.src('dist')
+                .pipe(clean());
+} )
+
+gulp.task('copy',['clean'], function(){
+    gulp.src(['src/components/**/*', 
+                'src/css/**/*', 
+                'src/javascript/**/*',
+                'src/imagens/**/*'], {"base": "src"})
+        .pipe(gulp.dest('dist'))
+
+})
 
 gulp.task('sass', function(){
     gulp.src('./src/sass/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./src/css/'));
+        .pipe(gulp.dest('./dist/css/'));
 })
 
 // gulp.task('listen', function(){
 //     gulp.watch('./src/sass/**/*.scss',['sass'])
 // })
 
-gulp.task('server', function(){
+gulp.task('html', function(){
+    return gulp.src('./src/index.html')
+        .pipe(include())
+        .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('server',['html'], function(){
     browserSync.init({
         server: {
-            baseDir: 'src'
+            baseDir: 'dist'
         }
     })
 
-    gulp.watch('./src/**/*').on('change', browserSync.reload)
+    gulp.watch('./dist/**/*').on('change', browserSync.reload)
 
     gulp.watch('./src/sass/**/*.scss',['sass'])
+    gulp.watch('./src/**/*.html',['html'])
 })
