7ee37cd541b20eb92231a722ecbdde19c2750ab6
abiliojunior <abiliojunior.mat@gmail.com>
2018-01-06 13:22:10 -0300

    Finalizacao do Menu

--- src/javascript/menu.js
+++ src/javascript/menu.js
@@ -3,7 +3,18 @@
 
     var btn = document.querySelector('.header-nav__hamburgger');
     var html = document.querySelector('html');
+    var menu = document.querySelector('#mainMenu');
+    var classMenu = 'menu-opened';
     var menuOpenneed = false;
+
+    html.addEventListener('click', function(e){
+        console.log(e.target);
+
+        if (e.target === html && menuOpenneed){
+            closeMenu();
+        }
+        
+    } )
 
     btn.addEventListener('click', toggleMenu);
 
@@ -17,15 +28,19 @@
 
     function closeMenu(){
         menuOpenneed = false;
-        html.classList.remove('menu-opened');
-
+        html.classList.remove(classMenu);
+        btn.blur();
+        menu.setAttribute('aria-expanded', false);
+        btn.setAttribute('aria-expanded', false);
     }
 
 
     
     function openMenu(){
         menuOpenneed = true;
-        html.classList.add('menu-opened');
+        html.classList.add(classMenu);
+        menu.setAttribute('aria-expanded', true);
+        btn.setAttribute('aria-expanded', true);
     }
 
 
